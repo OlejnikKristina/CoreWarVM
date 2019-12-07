8482082f72630ec723cf4d83b2189fe778a4af2e
Aidan Sullivan <asulliva@f1r4s1.codam.nl>
2019-12-07 14:27:55 +0100

    normed everything

--- SRCS_ASM/name_comment.c
+++ SRCS_ASM/name_comment.c
@@ -6,7 +6,7 @@
 /*   By: asulliva <asulliva@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/12/07 13:04:41 by asulliva       #+#    #+#                */
-/*   Updated: 2019/12/07 14:00:46 by asulliva      ########   odam.nl         */
+/*   Updated: 2019/12/07 14:27:43 by asulliva      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,31 +14,31 @@
 
 /*
 **  @desc   - function counts the quotes on the line with .name and .comment
-**  @param  - char *s, the .name or .comment string (one line) 
+**  @param  - char *s, the .name or .comment string (one line)
 **  @return - return 2 if 2 quotes in line
 **          - return 1 if 1 quote in line
 **          - return 0 if no quotes AKA and error
 */
 
-static int  count_quotes(char *s)
+static int	count_quotes(char *s)
 {
-    int i;
-    int quotes;
+	int	i;
+	int	quotes;
 
-    i = 0;
-    quotes = 0;
-    while (s[i])
-    {
-        if (s[i] == '"')
-            quotes++;
-        i++;
-    }
-    if (quotes > 1)
-        return (2);
-    else if (quotes > 0)
-        return (1);
-    else
-        return (0);
+	i = 0;
+	quotes = 0;
+	while (s[i])
+	{
+		if (s[i] == '"')
+			quotes++;
+		i++;
+	}
+	if (quotes > 1)
+		return (2);
+	else if (quotes > 0)
+		return (1);
+	else
+		return (0);
 }
 
 /*
@@ -48,29 +48,30 @@ static int  count_quotes(char *s)
 **          - int type, 0 if name, 1 if comment
 */
 
-static void multi_line(t_asm *data, char **s, int type)
+static void	multi_line(t_asm *data, char **s, int type)
 {
-    char    *line;
-    char    *check;
-    char    *temp;
+	char	*line;
+	char	*check;
+	char	*temp;
 
-    type = 0;
-    while (get_next_line(data->rfd, &line))
-    {
-        check = ft_strchr(line, '"');
-        if (check)
-            check = NULL;
-        if (ft_strstr(line, NAME_CMD_STRING) || ft_strstr(line, COMMENT_CMD_STRING))
-            error("No closing quote");
-        temp = ft_strjoin(*s, line);
-        free_arr(&line, NULL, 0);
-        *s = temp;
-        temp = NULL;
-        if (ft_strchr(*s, '"'))
-            break ;
-        *s = ft_strjoinone(*s, '\n');
-    }
-    ft_printf("[%s]\n", *s);
+	type = 0;
+	while (get_next_line(data->rfd, &line))
+	{
+		check = ft_strchr(line, '"');
+		if (check)
+			check = NULL;
+		if (ft_strstr(line, NAME_CMD_STRING) ||
+		ft_strstr(line, COMMENT_CMD_STRING))
+			error("No closing quote");
+		temp = ft_strjoin(*s, line);
+		free_arr(&line, NULL, 0);
+		*s = temp;
+		temp = NULL;
+		if (ft_strchr(*s, '"'))
+			break ;
+		*s = ft_strjoinone(*s, '\n');
+	}
+	ft_printf("[%s]\n", *s);
 }
 
 /*
@@ -83,21 +84,21 @@ static void multi_line(t_asm *data, char **s, int type)
 void		parse_nc(t_asm *data, char *s, int type)
 {
 	char	**split;
-    int     quotes;
+	int		quotes;
 
 	split = ft_strsplit(s, '"');
-    quotes = count_quotes(s);
-    if (!quotes)
-        error("Incorrect string in .name/.comment");
-    if (split[1])
-        s = ft_strdup(split[1]);
-    else if (!split[1])
-        s = ft_strdup("");
-    if (quotes == 1)
-    {
-        s = ft_strjoinone(s, '\n');
-        multi_line(data, &s, type);
-    }
-    free_arr(NULL, &split, 1);
+	quotes = count_quotes(s);
+	if (!quotes)
+		error("Incorrect string in .name/.comment");
+	if (split[1])
+		s = ft_strdup(split[1]);
+	else if (!split[1])
+		s = ft_strdup("");
+	if (quotes == 1)
+	{
+		s = ft_strjoinone(s, '\n');
+		multi_line(data, &s, type);
+	}
+	free_arr(NULL, &split, 1);
 	data->wfd = 0;
 }
