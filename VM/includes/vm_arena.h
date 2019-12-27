0a0424d2ce4c2cd46e07834ab22d11a453a2d4c9
Kristina <goncharenkokatja@gmail.com>
2019-12-27 17:44:44 +0100

    Put functions in one more file

--- VM/includes/vm_arena.h
+++ VM/includes/vm_arena.h
@@ -6,7 +6,7 @@
 /*   By: krioliin <krioliin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/12/20 15:52:12 by krioliin       #+#    #+#                */
-/*   Updated: 2019/12/23 17:53:05 by krioliin      ########   odam.nl         */
+/*   Updated: 2019/12/27 17:42:53 by krioliin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,6 +64,7 @@ bool				check_null_byte(const int fd);
 bool				get_player_code_size(t_player *player, const int fd);
 void				set_player_id(t_player *player, short players_order[MAX_PLAYERS],
 					short num);
+void				introduce_champions(t_player **players, short player_amnt);
 
 /******************************* Utilites ********************************/
 
