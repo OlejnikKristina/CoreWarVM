e8e1eeb5435bc8a59ac19cd330aafc01ba2324b7
Aidan Sullivan <asulliva@f1r2s21.codam.nl>
2020-01-07 18:59:01 +0100

    added AND op and ST op

--- VM/operations/op_st.c
+++ VM/operations/op_st.c
@@ -3,7 +3,22 @@
 
 bool		op_st(t_cursor *cursor, t_vm *vm)
 {
+	e_argctype	args[3];
+
+	decode_encoding_byte(vm->arena[cursor->pos + 1], args);
 	if (cursor && vm)
-		;
-	return (true);
+	{
+		if (args[0] == REG && args[1] == REG)
+		{
+			cursor->reg[vm->arena[cursor->pos + 3]] = 
+			cursor->reg[vm->arena[cursor->pos + 2]];
+		}
+		else if (args[0] == REG && args[1] == IND)
+		{
+			cursor->reg[vm->arena[cursor->pos + 3]] = 
+			convert(&vm->arena[cursor->pos + 1], 2) % IDX_MOD;
+		}
+		return (true);
+	}
+	return (false);
 }
