ce270a6e30a600a1522e3fa7a95cca4c57794c5c
Kristina <goncharenkokatja@gmail.com>
2020-01-08 18:16:53 +0100

    Marged with aidans operations

--- VM/utilities/convert.c
+++ VM/utilities/convert.c
@@ -6,11 +6,7 @@
 /*   By: asulliva <asulliva@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/12/30 16:35:54 by asulliva       #+#    #+#                */
-<<<<<<< HEAD
-/*   Updated: 2020/01/08 15:41:43 by krioliin      ########   odam.nl         */
-=======
-/*   Updated: 2020/01/07 16:25:24 by asulliva      ########   odam.nl         */
->>>>>>> aidan
+/*   Updated: 2020/01/08 18:16:28 by krioliin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
