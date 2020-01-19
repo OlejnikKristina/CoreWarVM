0d38e008a48e4596e59c042e89451c36c98ec364
Kristina <goncharenkokatja@gmail.com>
2020-01-19 15:29:22 +0100

    improve visual  st/sti

--- VM/visual/visual_sti.c
+++ VM/visual/visual_sti.c
@@ -6,11 +6,32 @@
 /*   By: krioliin <krioliin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/01/13 14:24:42 by krioliin       #+#    #+#                */
-/*   Updated: 2020/01/18 21:05:41 by krioliin      ########   odam.nl         */
+/*   Updated: 2020/01/19 15:28:29 by krioliin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "corewar_visual.h"
+
+void	visual_st(WINDOW *warena, uint8_t arena[], short id, int address)
+{
+	int		x;
+	int		y;
+	short	i;
+
+	i = 0;
+	wattron(warena, (COLOR_PAIR(id * 10 + 1) | A_DIM));
+	while (i < 4)
+	{
+		if (address >= MEM_SIZE)
+			address = 0;
+		get_xy_coordinates(&x, &y, address);
+		mvwprintw(warena, y, x, "%.2x ", arena[i]);
+		address++;
+		i++;
+	}
+	wrefresh(warena);
+	wattroff(warena, (COLOR_PAIR(id * 10 + 1) | A_DIM));
+}
 
 void	visual_sti(WINDOW *warena, uint8_t arena[], short id, int address)
 {
@@ -22,6 +43,8 @@ void	visual_sti(WINDOW *warena, uint8_t arena[], short id, int address)
 	wattron(warena, (COLOR_PAIR(id * 10 + 1) | A_DIM));
 	while (i < 4)
 	{
+		if (address >= MEM_SIZE)
+			address = 0;
 		get_xy_coordinates(&x, &y, address);
 		mvwprintw(warena, y, x, "%.2x ", arena[i]);
 		address++;
