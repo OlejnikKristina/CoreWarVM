7f87948bf6efd4b619fcd0d424f8bc9f1ec041f4
Kristina <goncharenkokatja@gmail.com>
2020-01-24 13:01:48 +0100

    try to fix bad repo

--- test2.sh
+++
@@ -1,9 +0,0 @@
-pl1="/Users/krioliin/Desktop/corewar/VM/examples/bee_gees.cor"
-i="3950"
-while [$i -lt $1]
-do
-	./VM/corewar pl1 -dump $i > diff/your_output
-	./corewar pl1 -dump $i > diff/origin_output
-	diff diff/onze_output diff/hun_output
-i=$[i + 1]
-done
